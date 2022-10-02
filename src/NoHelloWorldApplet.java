29b84572cf112b38efd133b9cab256e75ee11825
Andrew <cieslik.andrew@gmail.com>
2022-10-02 14:19:09 +0200

    no support for applets

--- src/NoHelloWorldApplet.java
+++ src/NoHelloWorldApplet.java
@@ -6,3 +6,15 @@ public class NoHelloWorldApplet extends Applet {
         g.drawString("No Hello world!", 50, 25);
     }
 }
+/*
+Here are browsers that do not support Java Applet any more:
+
+Google Chrome
+Firefox
+Safari
+Microsoft Edge
+Opera
+So, as you can see that all these browsers not longer support Applet. Even the newer versions of Oracle’s JDK does not come with the support of the Java browser plugin.
+
+The reason for dropping the support was because of security issues and risks that were found.
+ */
